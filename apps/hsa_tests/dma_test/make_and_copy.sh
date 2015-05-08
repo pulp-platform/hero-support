1043e803a5925a2bfc17bb9c03d39c9e59431916
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2015-05-08 11:20:04 +0200

    added new DMA test

---
+++ apps/hsa_tests/dma_test/make_and_copy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Name of the program
+NAME=dma_test
+
+# Make
+vivado-2014.1 make all
+
+# Copy
+# Host executable
+scp ${NAME} ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
+# Host sources for GDB
+scp *.c ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
+scp *.h ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
+# Accelerator binary
+scp *.bin ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
