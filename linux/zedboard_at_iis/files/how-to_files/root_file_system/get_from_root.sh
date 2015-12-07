5cc96b17dc13b842a983aaac8eee322095dd2a10
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2015-12-07 16:02:04 +0100

    added Conrad's fakeroot script

--- linux/zedboard_at_iis/files/how-to_files/root_file_system/get_from_root.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Copy back root file system from other machine
-echo "Copying root file system from ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/root_file_system/"
-scp ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/root_file_system/rootfs.cpio.gz .
--- linux/zedboard_at_iis/files/how-to_files/root_file_system/sourceme.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# machine where you have root privileges
-export SCP_TARGET_MACHINE="pirmin@iis-ubuntu2.ee.ethz.ch"
-
-# base path to which the move_to_root.sh scripts transfers the root file system for customization
-export SCP_TARGET_PATH="~/pulp_on_fpga/"
