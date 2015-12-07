0c5d11cf321b09e854460a22178be88d8ed5e2e0
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2015-12-07 15:49:02 +0100

    added Conrad's fakeroot script

--- linux/zedboard_at_iis/files/how-to_files/root_file_system/customize_fs.sh
+++ linux/zedboard_at_iis/files/how-to_files/root_file_system/customize_fs.sh
@@ -7,34 +7,40 @@
 #
 # Call using ./customize_fs.sh
 
-# Info
-echo "-----------------------------------------"
-echo "-        Customizing File System        -"
-echo "-----------------------------------------"
+# Enter fakeroot environment
+if [[ $EUID -ne 0 ]]
+then
+    echo "Entering fakeroot environment..."
+    fakeroot $0
+else
+    # Info
+    echo "-----------------------------------------"
+    echo "-        Customizing File System        -"
+    echo "-----------------------------------------"
+    
+    # Create a copy of the root file system
+    cp rootfs.cpio.gz `date +%F_%H-%M-%S`_rootfs.cpio.gz
+    
+    # Create a copy of the custom files
+    cp -r custom_files files_to_add
+    
+    # Change owner of files_to_add to root
+    chown -R root:root files_to_add
+    
+    # Unpack the root file system to tmp_mnt/
+    gunzip -c rootfs.cpio.gz | sh -c 'cd tmp_mnt/ && cpio -i'
 
-# Create a copy of the root file system
-cp rootfs.cpio.gz `date +%F_%H-%M-%S`_rootfs.cpio.gz
-
-# Create a copy of the custom files
-cp -r custom_files files_to_add
-
-# Change owner of files_to_add to root
-sudo chown -R root:root files_to_add
-
-# Unpack the root file system to tmp_mnt/
-gunzip -c rootfs.cpio.gz | sudo sh -c 'cd tmp_mnt/ && cpio -i'
-
-# Move content of files_to_add to unpacked file system
-sudo cp -r  files_to_add/* tmp_mnt/.
-
-# Repack the root file system
-sudo sh -c 'cd tmp_mnt/ && sudo find . | cpio -H newc -o' | gzip -9 > custom_rootfs.cpio.gz
-
-# Clean up
-sudo rm -rf files_to_add/
-sudo rm -rf tmp_mnt/*
-sudo mv custom_rootfs.cpio.gz rootfs.cpio.gz
-
-# Change ownership
-USER=`whoami`
-sudo chown ${USER}:${USER} *.cpio.gz
+    # bad fix for file /etc/dropbear
+    rm tmp_mnt/etc/dropbear
+    
+    # Move content of files_to_add to unpacked file system
+    cp -r  files_to_add/* tmp_mnt/.
+    
+    # Repack the root file system
+    sh -c 'cd tmp_mnt/ && find . | cpio -H newc -o' | gzip -9 > custom_rootfs.cpio.gz
+    
+    # Clean up
+    rm -rf files_to_add/
+    rm -rf tmp_mnt/*
+    mv custom_rootfs.cpio.gz rootfs.cpio.gz
+fi
