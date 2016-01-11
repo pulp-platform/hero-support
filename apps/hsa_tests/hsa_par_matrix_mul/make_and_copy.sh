9f3e0aa4bbc9a0cdb4c6bc0beab9d0bfce8be230
Conrad Burchert <bconrad@student.ethz.ch>
2016-01-11 22:46:56 +0100

    More make_and_copy.sh fixes for new sourceme variables

--- apps/hsa_tests/hsa_par_matrix_mul/make_and_copy.sh
+++ apps/hsa_tests/hsa_par_matrix_mul/make_and_copy.sh
@@ -7,13 +7,13 @@ APP=hsa_par_matrix_mul
 vivado-2015.1 make all
 
 # Create folder on target
-ssh ${SCP_TARGET_MACHINE} "mkdir -p ${SCP_TARGET_PATH}/programs/${APP}"
+ssh ${SCP_TARGET_MACHINE} "mkdir -p ${SCP_TARGET_PATH}/${APP}"
 
 # Copy
 # Host executable
-scp ${APP} ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/${APP}/.
+scp ${APP} ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${APP}/.
 # Host sources for GDB
-scp *.c ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/${APP}/.
-scp *.h ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/${APP}/.
+scp *.c ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${APP}/.
+scp *.h ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${APP}/.
 # Accelerator binary
-scp ${PULP_SW_PATH}/hsa_tests/${APP}/${APP}.bin ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/${APP}/.
+scp ${PULP_SW_PATH}/hsa_tests/${APP}/${APP}.bin ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${APP}/.
