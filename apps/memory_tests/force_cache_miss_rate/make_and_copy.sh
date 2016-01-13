b39cd23ab78d7353889b4257357dec4c37d99567
Conrad Burchert <bconrad@student.ethz.ch>
2016-01-13 19:16:09 +0100

    Makefile and make_and_copy fixes for new SDK

--- apps/memory_tests/force_cache_miss_rate/make_and_copy.sh
+++ apps/memory_tests/force_cache_miss_rate/make_and_copy.sh
@@ -8,9 +8,9 @@ ${VIVADO_VERSION} make all
 
 # Copy
 # Host executable
-scp ${NAME} ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
+scp ${NAME} ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${NAME}
 # Host sources for GDB
-scp *.c ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
-scp *.h ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
+scp *.c ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${NAME}
+scp *.h ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/${NAME}
 # Accelerator binary
 #scp *.bin ${SCP_TARGET_MACHINE}:${SCP_TARGET_PATH}/programs/.
