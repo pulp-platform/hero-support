2a04fddeb1f4f08ba65204c58124f607ccab7f74
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2016-04-25 17:42:35 +0200

    Revert "platform headers and sourcme modified"
    This reverts commit 0359f1e038f3fe8d5f0d5901ba33c01bc1003366

--- lib/inc/juno.h
+++
@@ -1,7 +0,0 @@
-#ifndef JUNO_H___
-#define JUNO_H___
-
-#define ARM_CLK_FREQ_MHZ 1100 // A57 overdrive
-#define DRAM_SIZE_MB 8192
-
-#endif // JUNO_H___
--- sourceme.sh
+++ sourceme.sh
@@ -9,66 +9,14 @@
 # Xilinx Vivado version for cross-compilation toolchain
 export VIVADO_VERSION="vivado-2015.1"
 
-# Select platform:
-#  ZEDBOARD: 1
-#  ZC706:    2
-#  MINI_ITX: 3
-#  JUNO:     4
-export PLATFORM="4"
+# system workspace directory
+export WORKSPACE_DIR=/scratch/vogelpi/mini-itx/workspace
 
-# Platform dependent variables
-if [ "${PLATFORM}" -eq "4" ]; then
-    echo "Configuring for JUNO platform"
-
-    # system workspace directory
-    export WORKSPACE_DIR=/usr/scratch/fliana/vogelpi/linaro-16.02/workspace
-
-    # directory containing the kernel sources
-    export KERNEL_DIR=${WORKSPACE_DIR}/linux
-
-    # machine to which the make_and_copy.sh scripts transfer the compiled stuff
-    export SCP_TARGET_MACHINE="juno@bordcomputer"
-
-    # base path to which the make_and_copy.sh scripts transfer the compiled stuff
-    export SCP_TARGET_PATH="~/juno/share/programs"
-    export SCP_TARGET_PATH_DRIVERS="~/juno/share/drivers"
-
-    # path to external ARM libraries (on scratch)
-    export ARM_LIB_EXT_DIR=/usr/scratch/fliana/vogelpi/libs-juno/lib
-
-    # number of cores on Linux host
-    export N_CORES_COMPILE=4
-else 
-    echo "Configuring for ZYNQ platform"
-
-    # system workspace directory
-    export WORKSPACE_DIR=/usr/scratch/riseten/vogelpi/mini-itx/workspace
-
-    # directory containing the kernel sources
-    export KERNEL_DIR=${WORKSPACE_DIR}/linux-xlnx
-
-    # machine to which the make_and_copy.sh scripts transfer the compiled stuff
-    export SCP_TARGET_MACHINE="vogelpi@bordcomputer"
-
-    # base path to which the make_and_copy.sh scripts transfer the compiled stuff
-    export SCP_TARGET_PATH="~/pulp_on_fpga/share/programs"
-    export SCP_TARGET_PATH_DRIVERS="~/pulp_on_fpga/share/drivers"
-
-    # path to external ARM libraries (on scratch)
-    export ARM_LIB_EXT_DIR=/scratch/vogelpi/libs-zynq/lib
-
-    # number of cores on Linux host
-    export N_CORES_COMPILE=8
-fi
+# directory containing the kernel sources
+export KERNEL_DIR=${WORKSPACE_DIR}/linux-xlnx
 
 # top directory containing custom ARM libraries
-export ARM_LIB_DIR1=/home/vogelpi/pulp_on_fpga/software/hsa_support/lib
-
-# create lib/lib folder if not existing
-if [ ! -d ${ARM_LIB_DIR1}/lib ]
-then
-    mkdir ${ARM_LIB_DIR1}/lib
-fi    
+export ARM_LIB_DIR1=/home/vogelpi/pulp_on_fpga/software/arm/lib
 
 # directory containing ARM header files
 export ARM_INC_DIR1=${ARM_LIB_DIR1}/inc
@@ -78,8 +26,26 @@ export PULP_INC_DIR1=/home/vogelpi/pulp_on_fpga/software/pulp/pulp_pipeline/pkg/
 export PULP_INC_DIR2=/home/vogelpi/pulp_on_fpga/software/pulp/pulp_pipeline/pkg/sdk/dev/install/include/pulp4
 
 # PULP HSA home directory
-export PULP_HSA_HOME=/home/vogelpi/pulp_on_fpga/software/hsa_apps
+export PULP_HSA_HOME=/home/vogelpi/pulp_on_fpga/software/hsa_sw
+
+# machine to which the make_and_copy.sh scripts transfer the compiled stuff
+export SCP_TARGET_MACHINE="vogelpi@bordcomputer"
+
+# base path to which the make_and_copy.sh scripts transfer the compiled stuff
+export SCP_TARGET_PATH="~/pulp_on_fpga/share/programs"
+export SCP_TARGET_PATH_DRIVERS="~/pulp_on_fpga/share/drivers"
+
+# create lib/lib folder if not existing
+if [ ! -d ${ARM_LIB_DIR1}/lib ]
+then
+    mkdir ${ARM_LIB_DIR1}/lib
+fi    
 
 # PULP binary path, i.e., where the compiled accelerator binaries can be found
-#export PULP_SW_PATH=/home/vogelpi/pulp_on_fpga/software/pulp
+export PULP_SW_PATH=/home/vogelpi/pulp_on_fpga/software/pulp
 
+# path to external ARM libraries (on scratch)
+export ARM_LIB_EXT_DIR=/scratch/vogelpi/libs-zynq/lib
+
+# number of cores on Linux host
+export N_CORES_COMPILE=8
