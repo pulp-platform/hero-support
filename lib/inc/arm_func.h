0359f1e038f3fe8d5f0d5901ba33c01bc1003366
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2016-04-25 17:30:42 +0200

    platform headers and sourcme modified

--- lib/inc/arm_func.h
+++
@@ -1,6 +0,0 @@
-#ifndef ARM_FUNC_H__
-#define ARM_FUNC_H__
-
-// -> zynq_pmm_user.h
-
-#endif // ARM_FUNC_H__
---
+++ lib/inc/juno.h
@@ -0,0 +1,7 @@
+#ifndef JUNO_H___
+#define JUNO_H___
+
+#define ARM_CLK_FREQ_MHZ 1100 // A57 overdrive
+#define DRAM_SIZE_MB 8192
+
+#endif // JUNO_H___
