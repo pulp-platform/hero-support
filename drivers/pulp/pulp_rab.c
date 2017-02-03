dbf7af611cc07a5fb077174ae3416ab4d4491306
Andreas Kurth <akurth@student.ethz.ch>
2017-02-03 10:01:11 +0100

    pulp_rab (soc_mh_ena): Remove unused arguments

--- drivers/pulp/pulp_rab.c
+++ drivers/pulp/pulp_rab.c
@@ -1898,7 +1898,7 @@ void pulp_rab_free_striped(void *rab_config, unsigned long arg)
 /**
  * TODO: documentation
  */
-int pulp_rab_soc_mh_ena(struct task_struct* task, void* rab_config, void* mbox)
+int pulp_rab_soc_mh_ena(void* rab_config)
 {
   unsigned long   pgd_pa;
   RabSliceReq     rab_slice_req;
