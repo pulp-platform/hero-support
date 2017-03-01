84417785a6227f2eb6cdb4af63edf00b746780d5
Andreas Kurth <akurth@student.ethz.ch>
2017-03-01 21:28:43 +0100

    pulp_func (rab_mh_disable): Update documentation to reflect changes to the underlying driver function

--- lib/src/pulp_func.c
+++ lib/src/pulp_func.c
@@ -917,7 +917,7 @@ void pulp_rab_mh_disable(const PulpDev *pulp)
  * RAB so that the page table hierarchy can be accessed from PULP.  For this, slices either for the
  * first-level page table or for all second-level page tables are configured in RAB (definable, see
  * parameter below).  Second, the driver function disables handling of RAB misses in the Kernel
- * driver itself.
+ * driver itself and prohibits the driver to write to those slices that are now managed by the SoC.
  *
  * @param   pulp                    Pointer to the PulpDev struct.
  * @param   static_2nd_lvl_slices   If 0, the driver sets up a single RAB slice for the first level
@@ -926,7 +926,8 @@ void pulp_rab_mh_disable(const PulpDev *pulp)
  *                                  all architectures.  If unsupported, the driver will fall back to
  *                                  the former behavior and emit a warning.
  *
- * @return  0 on success; negative value with an errno on errors.
+ * @return  0 on success; negative value with an errno on errors.  See the documentation of
+ *          `pulp_rab_soc_mh_ena()` for particular values.
  */
 int pulp_rab_soc_mh_enable(const PulpDev* pulp, const unsigned static_2nd_lvl_slices)
 {
