398aeab5aa64f764d6fba11943426572a37b627a
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2017-03-20 14:32:15 +0100

    zynq_pmm_user: moved definitions of inline functions to header, beautify

--- lib/inc/zynq_pmm_user.h
+++ lib/inc/zynq_pmm_user.h
@@ -21,8 +21,27 @@ int zynq_pmm_parse(char *buffer, long long *counters, int accumulate);
 int zynq_pmm_compute_rates(double *miss_rates, long long *counters);
 int zynq_pmm_print_rates(double *miss_rates);
 
-inline void arm_clk_cntr_reset();
-inline unsigned arm_clk_cntr_read();
 unsigned arm_clk_cntr_get_overhead();
+
+/*
+ * Reset the clock cycle counter in the PMU
+ */
+inline void arm_clk_cntr_reset()
+{
+  // enable clock counter divider (by 64), reset & enable clock counter, PMCR register
+  asm volatile("mcr p15, 0, %0, c9, c12, 0" :: "r"(0xD));
+}
+
+/*
+ * Read the clock cycle counter in the PMU
+ */
+inline unsigned arm_clk_cntr_read()
+{
+  unsigned value;
+  // Read the counter value, PMCCNTR register
+  asm volatile("mrc p15, 0, %0, c9, c13, 0" : "=r"(value) : );
+
+  return value;
+}
 
 #endif // ZYNQ_PMM_USER__
