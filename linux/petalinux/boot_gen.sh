c5a95ff127ce0f5bae8b012c8581fd23740a636a
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2017-06-02 16:25:33 +0200

    boot_gen.sh: disable pmu

--- linux/petalinux/boot_gen.sh
+++ linux/petalinux/boot_gen.sh
@@ -6,9 +6,12 @@
 cp ${PREBUILT_PATH}/u-boot.elf      ${IMAGE_PATH}/.
 cp ${PREBUILT_PATH}/zynqmp_fsbl.elf ${IMAGE_PATH}/.
 
+# Alternatively, patch the boot loader before the build
+cp -f ${WORKSPACE_PATH}/os/petalinux/components/bootloader/zynqmp_fsbl/xfsbl_board.te0808 ${WORKSPACE_PATH}/os/wimmis02/components/bootloader/zynqmp_fsbl/xfsbl_board.c
+
 # Copy bitstream
 cp ${BITSTREAM_PATH}/zusys_wrapper.bit ${IMAGE_PATH}/.
 
 # Go to iamge folder and generate
 cd ${IMAGE_PATH}
-petalinux-package --boot --fsbl zynqmp_fsbl.elf --fpga zusys_wrapper.bit --u-boot u-boot.elf --force
+petalinux-package --boot --fsbl zynqmp_fsbl.elf --fpga zusys_wrapper.bit --u-boot u-boot.elf --pmufw no --force
