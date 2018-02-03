a4a1bb73218727339eee2a6f0d822ca8f07b735c
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2018-02-03 17:08:55 +0100

    sourceme.sh: add TE0808 platform, adjust PULP include dirs for new SDK

--- sourceme.sh
+++ sourceme.sh
@@ -14,7 +14,8 @@ export VIVADO_VERSION="vivado-2015.1"
 #  ZC706:    2
 #  MINI_ITX: 3
 #  JUNO:     4
-export PLATFORM="4"
+#  TE0808:   5
+export PLATFORM="2"
 
 # Platform dependent variables
 
@@ -23,6 +24,7 @@ case ${PLATFORM} in
     2)  export BOARD="zc706";;
     3)  export BOARD="mini-itx";;
     4)  export BOARD="juno";;
+    5)  export BOARD="te0808";;
     *)
         echo "[EE] Failed to determine BOARD from PLATFORM!"
         exit 1
@@ -77,6 +79,50 @@ if [ "${PLATFORM}" -eq "4" ]; then
 	# directory containing PULP header files - on CentOS machine - needs to be accessible also by Ubuntu machine
 	export PULP_INC_DIR1=/home/vogelpi/riseten-scratch/juno/pulp_pipeline/pkg/sdk/dev/install/include/archi/bigpulp
 	export PULP_INC_DIR2=/home/vogelpi/riseten-scratch/juno/pulp_pipeline/pkg/sdk/dev/install/include
+elif [ "${PLATFORM}" -eq "5" ]; then
+    echo "Configuring for TE0808 ZYNQ MPSOC platform"
+
+    # system workspace directory - on CentOS machine
+    export WORKSPACE_DIR=/scratch/vogelpi/te0808
+
+    # directory containing the kernel sources
+    #export KERNEL_DIR=${WORKSPACE_DIR}/petalinux/TEBF0808/os/wimmis02/build/linux/kernel/link-to-kernel-build
+    #export KERNEL_DIR=/usr/scratch/larain4/vogelpi/te0808/petalinux/TEBF0808/os/wimmis02/components/ext_sources/linux-xlnx
+    #export KERNEL_DIR=/usr/scratch/larain4/vogelpi/te0808/petalinux/TEBF0808/os/wimmis02/build/tmp/work/plnx_aarch64-xilinx-linux/linux-xlnx/4.9-xilinx-v2017.2+git999-r0/linux-xlnx-4.9-xilinx-v2017.2+git999
+    export KERNEL_DIR=/scratch/vogelpi/te0808/petalinux/TEBF0808/os/wimmis02/build/tmp/work/plnx_aarch64-xilinx-linux/linux-xlnx/4.9-xilinx-v2017.2+git999-r0/linux-xlnx-4.9-xilinx-v2017.2+git999
+
+
+    # machine to which the make_and_copy.sh scripts transfer the compiled stuff
+    export SCP_TARGET_MACHINE="vogelpi@bordcomputer"
+
+    # base path to which the make_and_copy.sh scripts transfer the compiled stuff
+    export SCP_TARGET_PATH="~/zynqmp/share/apps"
+    export SCP_TARGET_PATH_DRIVERS="~/zynqmp/share/drivers"
+
+    # path to ARM libraries: external (opencv, ffmpeg), libc, shared libs in filesystem
+    export ARM_LIB_EXT_DIR=${WORKSPACE_DIR}/../libs-zynqmp/lib
+    #export ARM_LIBC_DIR=${WORKSPACE_DIR}/petalinux/TEBF0808/os/wimmis02/build/linux/rootfs/targetroot/lib
+    #export ARM_LIB_FS_DIR=${WORKSPACE_DIR}/petalinux/TEBF0808/os/wimmis02/build/linux/rootfs/targetroot/usr
+    export ARM_LIBC_DIR=/usr/scratch/larain4/vogelpi/te0808/petalinux/TEBF0808/os/wimmis02/build/tmp/sysroots/lib32-plnx_aarch64/lib
+    export ARM_LIB_FS_DIR=/usr/scratch/larain4/vogelpi/te0808/petalinux/TEBF0808/os/wimmis02/build/tmp/sysroots/lib32-plnx_aarch64/usr
+
+    # number of cores on Linux host
+    export N_CORES_COMPILE=8
+
+    # ARCH and CROSS_COMPILE variables
+    export KERNEL_ARCH="arm64"
+    export KERNEL_CROSS_COMPILE="aarch64-linux-gnu-"
+
+    export ARCH="arm"
+    export CROSS_COMPILE="arm-xilinx-linux-gnueabi-"
+    export TUPLE="arm-xilinx-linux-gnueabi"
+
+    # Set up PATH variable
+    #${VIVADO_VERSION} bash
+
+    # directory containing PULP header files - on CentOS machine
+	export PULP_INC_DIR1=${WORKSPACE_DIR}/pulp-sdk/pkg/sdk/dev/install/include/archi/chips/bigpulp
+	export PULP_INC_DIR2=${WORKSPACE_DIR}/pulp-sdk/pkg/sdk/dev/install/include
 else 
     echo "Configuring for ZYNQ platform"
 
@@ -119,8 +165,8 @@ else
     #${VIVADO_VERSION} bash
 
     # directory containing PULP header files - on CentOS machine
-	export PULP_INC_DIR1=${WORKSPACE_DIR}/pulp_pipeline/pkg/sdk/dev/install/include/archi/bigpulp
-	export PULP_INC_DIR2=${WORKSPACE_DIR}/pulp_pipeline/pkg/sdk/dev/install/include
+    export PULP_INC_DIR1=${WORKSPACE_DIR}/pulp-sdk/pkg/sdk/dev/install/include/archi/chips/bigpulp
+    export PULP_INC_DIR2=${WORKSPACE_DIR}/pulp-sdk/pkg/sdk/dev/install/include
 fi
 
 # top directory containing custom ARM libraries
