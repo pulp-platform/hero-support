2f79a69bb47fe80e4801f0de810f9a1833763705
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2018-02-13 17:16:47 +0100

    pulp_module.c: guard `mmap()` printf with debug flag

--- drivers/pulp/pulp_module.c
+++ drivers/pulp/pulp_module.c
@@ -30,7 +30,7 @@
  * 
  * Author: Pirmin Vogel - vogelpi@iis.ee.ethz.ch
  * 
- * Purpose : Linux kernel-level driver for bigPULP
+ * Purpose : Linux kernel-level driver for PULP
  *           RAB management for shared virtual memory between host and PULP
  * 
  * --=========================================================================--
@@ -821,9 +821,11 @@ int pulp_mmap(struct file *filp, struct vm_area_struct *vma)
   vma->vm_flags |= VM_IO | VM_RESERVED;
   vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 
-  printk(KERN_INFO
-    "PULP: %s memory mapped. \nPhysical address = %#lx, user-space virtual address = %#lx, vsize = %#lx.\n",
-    type, physical << PAGE_SHIFT, vma->vm_start, vsize);
+  if (DEBUG_LEVEL_PULP > 0) {
+    printk(KERN_INFO
+      "PULP: %s memory mapped. \nPhysical address = %#lx, user-space virtual address = %#lx, vsize = %#lx.\n",
+      type, physical << PAGE_SHIFT, vma->vm_start, vsize);
+  }
   
   if (vsize > psize)
     return -EINVAL; /*  spans too high */
