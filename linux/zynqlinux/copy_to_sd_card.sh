8697d83433f073fbb773fcc73aae6e7eaef28e52
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2018-08-16 19:00:24 +0200

    zynqlinux: do not copy the copy script to the SD card

--- linux/zynqlinux/copy_to_sd_card.sh
+++ linux/zynqlinux/copy_to_sd_card.sh
@@ -5,7 +5,10 @@
 SD_BOOT_PARTITION="/run/media/${USER}/ZYNQ_BOOT"
 
 # Copy to SD card
-cp * ${SD_BOOT_PARTITION}/.
+cp BOOT.bin          ${SD_BOOT_PARTITION}/.
+cp devicetree.dtb    ${SD_BOOT_PARTITION}/.
+cp uImage            ${SD_BOOT_PARTITION}/.
+cp uramdisk.image.gz ${SD_BOOT_PARTITION}/.
 
 # Unmount SD card
 sync
