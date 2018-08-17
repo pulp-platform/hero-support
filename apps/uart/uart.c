baa1661bee4caa1ffd06612769a5ff050847fe16
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2018-08-17 11:22:40 +0200

    apps/uart: print configured baudrate in any case

--- apps/uart/uart.c
+++ apps/uart/uart.c
@@ -1,5 +1,5 @@
 /*
- * PULP UART reader
+ * bigPULP UART reader
  *
  * Copyright 2018 ETH Zurich, University of Bologna
  *
@@ -38,17 +38,15 @@ int main(int argc, char *argv[]) {
 
   int baudrate = 115200; // PULP default
   int read_uart = 1;
-  
-  if (argc < 2) {
-    printf("*** PULP UART Reader ***\n");
-  }
-  else {
+
+  printf("*** bigPULP UART Reader ***\n");
+
+  if (argc > 1) {
     baudrate = atoi(argv[1]);
-    printf("Configuring for baudrate %d\n",baudrate);
-    
+
     if (argc > 2)
       read_uart = atoi(argv[2]);
-    
+
     if (argc > 3)
       printf("WARNING: More than 2 command line arguments are not supported. Those will be ignored.\n");
   }
@@ -65,25 +63,27 @@ int main(int argc, char *argv[]) {
   tio.c_ispeed = baudrate;
   tio.c_ospeed = baudrate;
 
+  printf("Configuring for baudrate %d\n",baudrate);
+
   if (ioctl(fd, TCSETS2, &tio) != 0) {
     printf("Failed to set baudrate.\n");
     return 1;
   }
-   
+
   if (read_uart) {
     printf("Will now start to read from /dev/ttyPS1. Hit Ctrl+C to exit.\n\n");
 
     i = 0;
     while (1) {
       int n = read(fd, &c, 1);
-  
+
       if (n == EINTR) {
         // we were interrupted, so we assume we should exit
         break;
-  
+
       } else if (n < 0) {
         printf("Read failed!\n");
-  
+
       } else if (n > 0) {
         if (i == (sizeof(buffer) - 1) || c == '\n') {
           buffer[i] = '\0';
