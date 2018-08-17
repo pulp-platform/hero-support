299cae831f3a01e3a232193cbb8e451c7d960be6
Pirmin Vogel <vogelpi@iis.ee.ethz.ch>
2018-08-17 12:58:35 +0200

    remove `ext-lib` scripts and makefiles, these are not needed for the release

--- ext-lib/configs/arm-xilinx-linux-gnueabi.toolchain.cmake
+++
@@ -1,9 +0,0 @@
-set( CMAKE_SYSTEM_NAME Linux )
-set( CMAKE_SYSTEM_PROCESSOR arm )
-set( CMAKE_C_COMPILER arm-xilinx-linux-gnueabi-gcc )
-set( CMAKE_CXX_COMPILER arm-xilinx-linux-gnueabi-g++ )
-
-#set( CMAKE_INSTALL_PREFIX /scratch/vogelpi/libs-zynq/libs )
-#set( CMAKE_FIND_ROOT_PATH /scratch/vogelpi/libs-zynq/libs \
-#     /scratch/vogelpi/libs-zynq/release  \
-#     /scratch/vogelpi/mini-itx/workspace/buildroot/output/target/usr/ )
