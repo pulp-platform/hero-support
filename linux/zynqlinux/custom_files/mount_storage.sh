40a430e21406d6ac3c6182b9f1375d774675a270
Andreas Kurth <akurth@iis.ee.ethz.ch>
2018-09-14 17:53:38 +0200

    zynqlinux/custom_files: Specify filesystem type when mounting SD card storage partition

--- linux/zynqlinux/custom_files/mount_storage.sh
+++ linux/zynqlinux/custom_files/mount_storage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Mount second partition on SD card if not mounted yet.
 if ! grep -qs '/mnt/storage ' /proc/mounts; then
-    mount -o noatime /dev/mmcblk0p2 /mnt/storage
+    mount -t ext2 -o noatime /dev/mmcblk0p2 /mnt/storage
 fi
